92a07d8014693fc8df1c5921a30b85d3e40774a8
Loannes <Loannes@Loannes-ui-MacBook-Pro.local>
2015-05-02 15:20:56 +0900

    Set Environment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Loooog::Application.config.secret_key_base = '219fad6243dead588bf04b18c40c36ff635385e989b7a34c5ef92a9ba6fa43e99290411420320c0c97b99a58455bdf9be61a6c0719cda108e31e649826b859dc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Loooog::Application.config.session_store :cookie_store, key: '_loooog_session'
